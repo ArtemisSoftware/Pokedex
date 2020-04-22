26f3c91b711959c2a49d466b6977fe4d2f97ed66
gustavo <gustavo.maia@b2b.com.pt>
2020-04-22 11:17:49 +0100

    Class ApiConstants

---
+++ app/src/main/java/com/artemissoftware/pokedex/util/ApiConstants.java
@@ -0,0 +1,9 @@
+package com.artemissoftware.pokedex.util;
+
+public class ApiConstants {
+
+    public static final String BASE_URL = "https://pokeapi.co/api/v2/";
+
+    public static final String DETAIL_URL = "https://pokeapi.glitch.me/v1/";
+
+}

e137888fa0a7a12c64bae87d060ac9636dfae03c
gustavo <gustavo.maia@b2b.com.pt>
2020-04-22 11:04:49 +0100

    PokemonResults request model

---
+++ app/src/main/java/com/artemissoftware/pokedex/requests/models/PokemonResults.java
@@ -0,0 +1,25 @@
+package com.artemissoftware.pokedex.requests.models;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class PokemonResults {
+
+    @SerializedName("count")
+    public String count;
+
+    @SerializedName("results")
+    public List<PokemonInfo> pokemons;
+
+
+    public class PokemonInfo{
+
+        @SerializedName("id")
+        public String id;
+
+        @SerializedName("name")
+        public String name;
+    }
+
+}

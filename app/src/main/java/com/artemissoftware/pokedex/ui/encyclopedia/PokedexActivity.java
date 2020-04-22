e68eeeb5c59c156e1d1313d1c9386b67636815d1
gustavo <gustavo.maia@b2b.com.pt>
2020-04-22 15:17:20 +0100

    Layout activity_pokedex

---
+++ app/src/main/java/com/artemissoftware/pokedex/ui/encyclopedia/PokedexActivity.java
@@ -0,0 +1,16 @@
+package com.artemissoftware.pokedex.ui.encyclopedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.artemissoftware.pokedex.R;
+
+public class PokedexActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pokedex);
+    }
+}

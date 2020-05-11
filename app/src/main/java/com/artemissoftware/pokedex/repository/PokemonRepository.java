b188f59e91cce1a92cac6f6af5f089b05946af82
gustavo <gustavo.maia@b2b.com.pt>
2020-05-11 09:26:58 +0100

    Dependencie shinebutton

--- app/src/main/java/com/artemissoftware/pokedex/repository/PokemonRepository.java
+++ app/src/main/java/com/artemissoftware/pokedex/repository/PokemonRepository.java
@@ -43,6 +43,18 @@ public class PokemonRepository {
         notes.add(new Note(1, "Note two", new Date()));
         notes.add(new Note(1, "Note three", new Date()));
 
+        notes.add(new Note(1, "Note one one", new Date()));
+        notes.add(new Note(1, "Note two one", new Date()));
+        notes.add(new Note(1, "Note thre eone", new Date()));
+
+        notes.add(new Note(1, "Note one two", new Date()));
+        notes.add(new Note(1, "Note two two", new Date()));
+        notes.add(new Note(1, "Note three two", new Date()));
+
+        notes.add(new Note(1, "Note one three", new Date()));
+        notes.add(new Note(1, "Note two three", new Date()));
+        notes.add(new Note(1, "Note three three", new Date()));
+
         return notes;
         //return categoryDao.getCategories();
     }

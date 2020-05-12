5c5431308f15c7c732aeaa3988f7a47db8f9eff6
gustavo <gustavo.maia@b2b.com.pt>
2020-05-12 10:26:26 +0100

    DataBase contants class

---
+++ app/src/main/java/com/artemissoftware/pokedex/util/DataBase.java
@@ -0,0 +1,8 @@
+package com.artemissoftware.pokedex.util;
+
+public class DataBase {
+
+    public static final String DATABASE_NAME = "pokemon_db";
+    public static final int DATABASE_VERSION = 1;
+
+}

e915ae857efa521cb1f76fd3dff5b3601c0f8ba9
gustavo <gustavo.maia@b2b.com.pt>
2020-05-13 14:43:02 +0100

    Class MigrationDb

---
+++ app/src/main/java/com/artemissoftware/pokedex/database/MigrationDb.java
@@ -0,0 +1,40 @@
+package com.artemissoftware.pokedex.database;
+
+import android.database.SQLException;
+
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import timber.log.Timber;
+
+public class MigrationDb {
+
+    public static final Migration  [] getMigrations(){
+
+        Migration migrations [] =  new Migration []{
+                MIGRATION_1_2
+        };
+
+        return migrations;
+    }
+
+
+    public static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+            try {
+                database.execSQL("CREATE TABLE IF NOT EXISTS 'pokemons' ("
+                        + "'id' TEXT PRIMARY KEY , "
+                        + "'name' TEXT NOT NULL, "
+                        + "'description' TEXT NOT NULL) ");
+
+                Timber.d("MIGRATION_1_2: success");
+            }
+            catch(SQLException e){
+                Timber.e("erro MIGRATION_1_2: " + e.getMessage());
+            }
+        }
+    };
+
+
+}
--- app/src/main/java/com/artemissoftware/pokedex/ui/pokemon/models/Pokemon.java
+++ app/src/main/java/com/artemissoftware/pokedex/ui/pokemon/models/Pokemon.java
@@ -1,5 +1,7 @@
 package com.artemissoftware.pokedex.ui.pokemon.models;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
@@ -9,10 +11,19 @@ public class Pokemon {
     @PrimaryKey
     private String id;
 
+    @NonNull
+    @ColumnInfo(name = "name")
+    private String name;
+
+    @ColumnInfo(name = "description")
+    private String description;
+
     public Pokemon() {}
 
-    public Pokemon(String id) {
+    public Pokemon(String id, @NonNull String name, String description) {
         this.id = id;
+        this.name = name;
+        this.description = description;
     }
 
 
@@ -22,5 +33,22 @@ public class Pokemon {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    @NonNull
+    public String getName() {
+        return name;
+    }
+
+    public void setName(@NonNull String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }

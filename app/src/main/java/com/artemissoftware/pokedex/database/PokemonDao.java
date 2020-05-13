643c8d3516a12d24559cb2b892d35e76a50ea57e
gustavo <gustavo.maia@b2b.com.pt>
2020-05-13 12:37:10 +0100

    PokemonDao

---
+++ app/src/main/java/com/artemissoftware/pokedex/database/PokemonDao.java
@@ -0,0 +1,22 @@
+package com.artemissoftware.pokedex.database;
+
+import androidx.room.Dao;
+import androidx.room.Query;
+
+import com.artemissoftware.pokedex.ui.pokemon.models.Pokemon;
+
+import java.util.List;
+
+import io.reactivex.Flowable;
+import io.reactivex.Maybe;
+
+@Dao
+abstract public class PokemonDao implements BaseDao<Pokemon>{
+
+    @Query("SELECT COUNT(id) FROM pokemons WHERE id = :id")
+    abstract public Maybe<Integer> getAll(String id);
+
+
+    @Query("SELECT * FROM pokemons ORDER BY id ASC")
+    abstract public Flowable<List<Pokemon>> getAll();
+}
